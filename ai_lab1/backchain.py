ebb841c431f0493c5ac0dbae07180f6303162a18
Zac Yauney <zac.yauney@gmail.com>
2016-07-21 16:24:40 -0700

    Finished lab 1

--- ai_lab1/backchain.py
+++ ai_lab1/backchain.py
@@ -15,8 +15,36 @@ from zookeeper import ZOOKEEPER_RULES
 
 
 def backchain_to_goal_tree(rules, hypothesis):
-    raise NotImplementedError
+    # for rule in rules:
+        # print rule.antecedent()
+        # print rule.consequent()
+    if not rules:
+        return hypothesis
+    return simplify(rule_match_goal_tree(rules, hypothesis))
+
+def rule_match_goal_tree(rules, hypothesis):
+    # matches = filter(lambda rule: rule_has_match(rule, hypothesis), rules)
+    # return OR([antecedents_goal_tree(item, rules, match(item.consequent()[0], hypothesis)) for item in matches])
+    subtrees = []
+    for rule in rules:
+        binding = match(rule.consequent()[0], hypothesis)
+        if binding is not None:
+            subtrees.append(antecedents_goal_tree(rule, rules, binding))
+        else:
+            subtrees.append(hypothesis)
+    return OR(subtrees)
+
+def antecedents_goal_tree(rule, rules, binding):
+    ant = rule.antecedent()
+    new_hypothesis = populate(ant, binding)
+    if not (isinstance(ant, AND) or isinstance(ant, OR)):
+        return rule_match_goal_tree(rules, new_hypothesis)
+    subtrees = [rule_match_goal_tree(rules, item) for item in new_hypothesis]
+    if isinstance(ant, AND):
+        return AND(subtrees)
+    else: 
+        return OR(subtrees)
 
 # Here's an example of running the backward chainer - uncomment
 # it to see it work:
-#print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
+# print backchain_to_goal_tree(ZOOKEEPER_RULES, 'opus is a penguin')
